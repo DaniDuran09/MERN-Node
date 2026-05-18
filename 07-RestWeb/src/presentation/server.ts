3a67e3c67b353db084deaf2441e31d77b7c3dce1
DaniDuran09 <daniel.duran.torres09@gmail.com>
2026-05-18 10:21:24 -0600

    feat: initialize new web project with server configuration and hero asset gallery

---
+++ 07-RestWeb/src/presentation/server.ts
@@ -0,0 +1,44 @@
+import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+interface Options {
+    port: number;
+    public_path: string;
+}
+
+export class Server {
+
+    private app = express();
+
+    private readonly port: number;
+    private readonly publicPath: string
+
+    constructor(options: Options) {
+        const { port, public_path } = options;
+        this.port = port;
+        this.publicPath = public_path;
+    }
+
+    async start() {
+
+        //* Middlewares
+
+        // public foler
+        this.app.use(express.static('public'));
+
+
+        this.app.use((req, res) => {
+            const indexPath = path.resolve(__dirname, '../../public/index.html');
+            res.sendFile(indexPath);
+        });
+
+
+        this.app.listen(3000, () => {
+            console.log('Server running on port 3000');
+        })
+    }
+}
