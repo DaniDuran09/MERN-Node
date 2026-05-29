1cee32ac461d56a5deec39097a055defa643f777
DaniDuran09 <daniel.duran.torres09@gmail.com>
2026-05-29 10:44:04 -0600

    feat: implement category and product models, authentication middleware, and category management routes

---
+++ 08-user-store/src/domain/dtos/category/create-category.dto.ts
@@ -0,0 +1,22 @@
+
+
+export class CreateCategoryDto {
+
+    private constructor(
+        public readonly name: string,
+        public readonly available: boolean,
+    ) { }
+
+    static create(object: { [key: string]: any }): [string?, CreateCategoryDto?] {
+        const { name, available = false } = object;
+        let availablebolean = available;
+
+        if (!name) return ['Name is required'];
+        if (typeof available !== 'boolean') {
+            availablebolean = (available === 'true')
+        }
+
+        return [undefined, new CreateCategoryDto(name, availablebolean)];
+    }
+}
+
